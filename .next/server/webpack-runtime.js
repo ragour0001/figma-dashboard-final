1ccf2365a039d5f174ac99ca62377a3f8de7eee6
Rakesh Gour <ragour19@in.ibm.com>
2025-06-13 19:58:06 +0530

    removed webpack stuff

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("f9cc02eb28efbdbb")
+/******/ 		__webpack_require__.h = () => ("201bcae00a898213")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "10b6331d2313cbb5"; }
+/******/ 		__webpack_require__.h = function() { return "417acf5f1d6049e5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
