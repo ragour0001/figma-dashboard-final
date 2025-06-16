2552929124a6b266c8b90d6344b922ccfc6551cd
Rakesh Gour <ragour19@in.ibm.com>
2025-06-17 00:54:28 +0530

    added ios and android app images

--- app/components/TherapistCoach.tsx
+++ app/components/TherapistCoach.tsx
@@ -173,12 +173,13 @@ function UpcomingSessionDetails() {
       <div className="session-header">
         <div className="therapist-session-info">
           <img
-            src="https://placehold.co/78x78/E0E0E0/E0E0E0"
+            // src="https://placehold.co/78x78/E0E0E0/E0E0E0"
+            src="/assets/images/Avatar.png"          
             alt="Mridul Sharma"
             className="session-therapist-avatar"
           />
           <div className="session-therapist-details">
-            <h3 className="session-therapist-name">Mridul Sharma</h3>
+            <h3 className="session-therapist-name">Olivia Rhye</h3>
             <p className="session-description">30 min meeting with</p>
           </div>
         </div>
@@ -377,7 +378,9 @@ function NewPlatformSection() {
           <div className="download-section">
             <h3 className="download-title">Download Apps:</h3>
             <div className="download-buttons">
-              <button className="app-store-btn">
+            <img src="/assets/images/ios_image.png" alt="phone-1" />
+            <img src="/assets/images/android_image.png" alt="phone-1" />
+              {/* <button className="app-store-btn">
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
                   <path
                     d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.81-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M13 3.5c.73-.83 1.94-1.46 2.94-1.5.13 1.17-.34 2.35-1.04 3.19-.69.85-1.83 1.51-2.95 1.42-.15-1.15.41-2.35 1.05-3.11z"
@@ -400,7 +403,7 @@ function NewPlatformSection() {
                   <span className="store-line1">GET IT ON</span>
                   <span className="store-line2">Google Play</span>
                 </div>
-              </button>
+              </button> */}
             </div>
           </div>
         </div>
